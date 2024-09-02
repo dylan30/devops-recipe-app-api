f6bfa19aa4d82ba8494a0d8ceeba9ba263e2a901
dylan_dai <dylan_dai@hotmail.com>
2024-09-02 12:20:35 -0600

    deploy terraform validated

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -1,36 +1,36 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "5.23.0"
+      source  = "hashicorp/aws"
+      version = "5.23.0"
     }
   }
 
   backend "s3" {
-    bucket = "devops2-recipe-app-tf-state"
-    key = "tf-state-deploy"
+    bucket               = "devops2-recipe-app-tf-state"
+    key                  = "tf-state-deploy"
     workspace_key_prefix = "tf-state-deploy-env"
-    region = "us-east-1"
-    encrypt = true
-    dynamodb_table = "devops2-recipe-app-tf-lock"
+    region               = "us-east-1"
+    encrypt              = true
+    dynamodb_table       = "devops2-recipe-app-tf-lock"
   }
 }
 
 provider "aws" {
   region = "us-east-1"
   default_tags {
-    tags ={
-        Environment = terraform.workspace
-        Project = var.project
-        Contact = var.contact
-        ManageBy = "Terraform/deploy"
-    } 
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      Contact     = var.contact
+      ManageBy    = "Terraform/deploy"
+    }
 
   }
 }
 
-locals{
-    prefix = "${var.prefix}-${terraform.workspace}"
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
 }
 
-data "aws_region" "current"{}
+data "aws_region" "current" {}
--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -1,14 +1,14 @@
-variable "prefix"{
-    description = "prefix for resources in AWS"
-    default = "raa"
+variable "prefix" {
+  description = "prefix for resources in AWS"
+  default     = "raa"
 }
 
 variable "project" {
   description = "project name for tagging resources"
-  default = "recipe-app-api"
+  default     = "recipe-app-api"
 }
 
 variable "contact" {
   description = "contact email for tagging resources"
-  default = "dylan_dai@hotmail.com"
+  default     = "dylan_dai@hotmail.com"
 }
